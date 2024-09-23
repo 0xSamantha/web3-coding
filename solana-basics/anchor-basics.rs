e3b890c34112825b5676e72ac2c779ef6aff06b9
samantha <samchavoya@gmail.com>
2024-09-23 12:18:25 -0700

    add: more solana terminal instructions

--- solana-basics/anchor-basics.rs
+++ solana-basics/anchor-basics.rs
@@ -3,3 +3,12 @@
 // anchor generates Interface Description Language (IDL) files, which describle the program's structure & functionality + enables creation of JS/TS client libraries.
 // uses parallel execution
 // solana uses rent
+
+
+// set default solana configuration:
+// solana config get
+
+// change configuration to use local cluster:
+// solana config set --url localhost
+
+
